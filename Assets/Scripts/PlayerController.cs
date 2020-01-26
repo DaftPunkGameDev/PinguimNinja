7b2969ab80b525eee275214c9af5b28a57c0e05b
Matheus Reis <matheus.reisluz@gmail.com>
2020-01-25 22:13:52 -0300

    Beta 0.1 - Início

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+    public float jumpForce;
+
+    private Rigidbody2D myRigidBody;
+
+    public bool grounded;
+    public LayerMask whatIsGround;
+
+    private Collider2D myCollider;
+
+    private Animator myAnimator; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidBody = GetComponent <Rigidbody2D>();
+        myCollider = GetComponent <Collider2D>();
+        myAnimator = GetComponent <Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        grounded = Physics2D.IsTouchingLayers(myCollider, whatIsGround);
+
+        myRigidBody.velocity = new Vector2(moveSpeed, myRigidBody.velocity.y);
+
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+        {
+            if (grounded) 
+            { 
+                myRigidBody.velocity = new Vector2(myRigidBody.velocity.x, jumpForce);
+            }
+        }
+
+        myAnimator.SetFloat("Speed", myRigidBody.velocity.x);
+        myAnimator.SetBool("Grounded", grounded);
+    }
+}
